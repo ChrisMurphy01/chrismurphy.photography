ee902818fbb7cc248173859e9207c2237acd6abc
Chris Murphy <chrismurphy01@gmail.com>
2019-03-16 19:44:22 +0000

    lmabda test

--- lambda-src/purchase.js
+++ lambda-src/purchase.js
@@ -55,6 +55,7 @@ exports.handler = function(event, context, callback) {
           ? 'failed'
           : charge.status
       console.log('lambda', { err }, { charge }, '\n', '\n')
+      charge = charge.json()
       callback(null, {
         statusCode,
         headers,
